c7ff79f106fe07436ca00c42e7c0eb7bd5db2eca
Tanuja Savadatti <tanuja.1si22cs185@gmail.com>
2025-12-16 09:00:57 +0530

    Add user input for matrix multiplication

--- 2D_arrays/challenge60/multiply.py
+++ 2D_arrays/challenge60/multiply.py
@@ -1,5 +1,5 @@
 def multiply_matrices(a, b):
-    result = [[0]*len(b[0]) for _ in range(len(a))]
+    result = [[0] * len(b[0]) for _ in range(len(a))]
 
     for i in range(len(a)):
         for j in range(len(b[0])):
@@ -9,9 +9,31 @@ def multiply_matrices(a, b):
 
 
 def main():
-    a = [[1,2],[3,4]]
-    b = [[5,6],[7,8]]
-    print("Result:", multiply_matrices(a, b))
+    r1 = int(input("Enter number of rows of first matrix: "))
+    c1 = int(input("Enter number of columns of first matrix: "))
+
+    print("Enter elements of first matrix:")
+    a = []
+    for i in range(r1):
+        row = list(map(int, input().split()))
+        a.append(row)
+
+    r2 = int(input("Enter number of rows of second matrix: "))
+    c2 = int(input("Enter number of columns of second matrix: "))
+
+    print("Enter elements of second matrix:")
+    b = []
+    for i in range(r2):
+        row = list(map(int, input().split()))
+        b.append(row)
+
+    # Matrix multiplication condition
+    if c1 != r2:
+        print("Matrix multiplication not possible")
+        return
+
+    result = multiply_matrices(a, b)
+    print("Result:", result)
 
 
 if __name__ == "__main__":
